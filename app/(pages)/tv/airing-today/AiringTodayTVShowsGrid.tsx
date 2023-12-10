04905aa3e576c8a449834b1fceeb8fda97d3df63
Bobbaru <bobbaru_krueger@yahoo.com>
2023-12-10 21:44:04 +0200

    prefetch airing today tv shows

---
+++ app/(pages)/tv/airing-today/AiringTodayTVShowsGrid.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { TVShowsCard } from "@/app/components/Card/TVShow";
+import { AIRING_TODAY_MOVIES_KEY } from "@/app/constants";
+import { TVShowsResponse } from "@/app/types/tv/TVShowsResponse";
+import moviesFetchConfig from "@/app/utils/moviesFetchConfig";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  page: number;
+  with_original_language: string;
+  sort_by: string;
+}
+
+const AiringTodayTVShowsGrid = ({
+  page,
+  with_original_language,
+  sort_by,
+}: Props) => {
+  const tvShowConfig = moviesFetchConfig(page, with_original_language, sort_by);
+
+  // const apiClient = new APIClient<MoviesResponse>(DISCOVER_MOVIES_ENDPOINT);
+
+  const { data, error, isLoading } = useQuery<TVShowsResponse>({
+    queryKey: [AIRING_TODAY_MOVIES_KEY, tvShowConfig.params],
+    // queryFn: () => apiClient.getAll(moviesConfig),
+    placeholderData: keepPreviousData,
+  });
+
+  const router = useRouter();
+
+  if (error) throw new Error("Something went wrong while fetching 'Popular TV Shows'");
+
+  if (isLoading)
+    return <div className="alert alert-info">Loading movies...</div>;
+
+  return (
+    <>
+      <div className="flex gap-4 items-center justify-between mb-4">
+        <button
+          className="btn btn-primary"
+          disabled={tvShowConfig.params.page <= 1}
+          onClick={() =>
+            router.push(
+              `/tv/airing-today?page=${Number(tvShowConfig.params.page) - 1}${
+                with_original_language
+                  ? "&with_original_language=" + with_original_language
+                  : ""
+              }${sort_by ? "&sort_by=" + sort_by : ""}`
+            )
+          }>
+          Prev
+        </button>
+        {data?.page} of {data?.total_pages} / {data?.total_results} results
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            router.push(
+              `/tv/airing-today?page=${Number(tvShowConfig.params.page) + 1}${
+                with_original_language
+                  ? "&with_original_language=" + with_original_language
+                  : ""
+              }${sort_by ? "&sort_by=" + sort_by : ""}`
+            );
+          }}>
+          Next
+        </button>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3">
+        {data?.results?.map((tvShow) => (
+          <TVShowsCard key={tvShow.id} tvShow={tvShow} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AiringTodayTVShowsGrid;
--- app/constants.ts
+++ app/constants.ts
@@ -1,18 +1,21 @@
 export const STALE_TIME = 24 * 60 * 60 * 1000; // 24 hours
 
 // Movies
-export const POPULAR_MOVIES_KEY = "popular_movies";
+export const POPULAR_MOVIES_KEY = "popular-movies";
 export const POPULAR_MOVIES_ENDPOINT = "discover/movie";
 
-export const NOW_PLAYING_MOVIES_KEY = "now_playing_movies";
+export const NOW_PLAYING_MOVIES_KEY = "now_playing-movies";
 export const NOW_PLAYING_MOVIES_ENDPOINT = "movie/now_playing";
 
-export const TOP_RATED_MOVIES_KEY = "top_rated_movies";
+export const TOP_RATED_MOVIES_KEY = "top_rated-movies";
 export const TOP_RATED_MOVIES_ENDPOINT = "movie/top_rated";
 
-export const UPCOMING_MOVIES_KEY = "upcoming_movies";
+export const UPCOMING_MOVIES_KEY = "upcoming-movies";
 export const UPCOMING_MOVIES_ENDPOINT = "movie/upcoming";
 
 // TV shows
-export const POPULAR_TVSHOWS_KEY = "popular_tvshows";
+export const POPULAR_TVSHOWS_KEY = "popular-tvshows";
 export const POPULAR_TVSHOWS_ENDPOINT = "discover/tv";
+
+export const AIRING_TODAY_MOVIES_KEY = "airing_today-tvshows";
+export const AIRING_TODAY_MOVIES_ENDPOINT = "tv/airing_today";
