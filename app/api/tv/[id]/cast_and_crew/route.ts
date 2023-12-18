56799e496e5495aa725e69c8a0e4054a8c579847
Bobbaru <bobbaru_krueger@yahoo.com>
2023-12-18 23:35:31 +0200

    my cast and crew apis

---
+++ app/api/tv/[id]/cast_and_crew/route.ts
@@ -0,0 +1,15 @@
+import { CastAndCrew } from "@/app/types/movies/CastAndCrew";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<NextResponse<CastAndCrew>> {
+  const id = params.id;
+
+  const castAndCrew: CastAndCrew = await fetch(
+    `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${process.env.TMDB_API_KEY}`
+  ).then((res) => res.json());
+
+  return NextResponse.json(castAndCrew);
+}
