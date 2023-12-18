b3af08f4905b93d190cb4f660ecb7128a350c1a8
Bobbaru <bobbaru_krueger@yahoo.com>
2023-12-18 23:28:02 +0200

    my alternative titles apis

---
+++ app/api/movie/[id]/alternative_titles/route.ts
@@ -0,0 +1,15 @@
+import { MovieAlternativeTitles } from "@/app/types/movies/movie/MovieAlternativeTitles";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<NextResponse<MovieAlternativeTitles>> {
+  const id = params.id;
+
+  const alternativeTitles: MovieAlternativeTitles = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}/alternative_titles?api_key=${process.env.TMDB_API_KEY}`
+  ).then((res) => res.json());
+
+  return NextResponse.json(alternativeTitles);
+}
